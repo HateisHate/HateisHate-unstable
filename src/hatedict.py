d1256264266c0d44545aab77e0a7b3de2824ec1e
Aaron Thomas <1d20m6@gmail.com>
2016-11-05 17:14:45 -0400

    added a dictionary of hateful words.

---
+++ src/hatedict.py
@@ -0,0 +1,96 @@
+#!/usr/bin/python
+'''
+Don't call people any of these words, it's not nice.
+This list is incomplete, contributions are welcome.
+'''
+
+HATEDICT = {"sexism_misogyny" : ["maid",
+								"slut",
+								"whore",
+								"bitch",
+								"cumslut" #slut is in cumslut, might be redundant
+				],
+			"sexism_misandry" : ["no balls",
+								"pussy"
+				],
+			"sexism_transphobia" : ["tranny"
+				],
+			"sexism_heterophobia" : ["het",
+								"vanilla"
+				],
+			"sexism_homophobia" : ["fairy",
+								"faggot"
+				],
+			"racism_asain" : ["chink",
+								"squinty",
+								"kimcho",
+								"curry-muncher",#just curry?
+								"ching-chong",#ching.chong so it matches space or hyphen?
+								"gook",
+								"yigger",
+								"winky",
+								"rice nigger"
+				],
+			"racism_african" : ["nigger",
+								"spook",
+								"crow",
+								"porch monkey",#just monkey?
+								"coon",
+								"mosshead",
+								"blue gums",
+								"sausage lips"
+				],
+			"racism_caucasian" : ["wigger",
+								"whitey",
+								"cracker",
+								"klansman",
+								"cornfed",
+								"white bread",
+								"redneck"
+				],
+			"racism_middle-eastern" : ["turban"
+								"towel head",
+								"sand nigger"
+				],
+			"racism_antisemitism" : ["abe",
+								"big nose",
+								"kike",
+								"kyke",
+								"goyim",
+								"oven-dweller"
+				],
+			"racism_pacific" : [""
+				],
+			"racism_australian" : [""
+				] ,
+			"racism_native_american": ["prarie nigger",
+								"savage",
+								"redskin",
+								"injun",
+								"half-breed",#use a . for regex?
+								"chug"
+				]
+	
+}
+
+FLAVOURS = ["sexism_misogyny",
+			"sexism_misandry",
+			"sexism_transphobia",
+			"sexism_heterophobia",
+			"sexism_homophobia",
+			"racism_asain",
+			"racism_african",
+			"racism_caucasian",
+			"racism_middle-eastern",
+			"racism_antisemitism",
+			"racism_pacific",
+			"racism_australian",
+			"racism_native_american"]
+
+#returns the hatedictionary
+def dict():
+	return HATEDICT
+
+#returns a list of the flavours of hate the hatedictionary currently supports
+def flavours():
+	return FLAVOURS
