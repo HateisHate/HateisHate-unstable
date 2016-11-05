e07b8c67563463624062fe7161b6f51f0b0377b4
Aaron Thomas <1d20m6@gmail.com>
2016-11-05 14:29:41 -0400

    updated README.md, added flavours.py, added hateishate.py

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
-# HateisHate
+# @HateisHate_
+A twitter bot that changes the flavour of hateful tweets.
+
+<h3>Dependencies</h3>
+<ul>
+	<li>python3 https://www.python.org/</li>
+</ul>
---
+++ src/flavors.py
@@ -0,0 +1,36 @@
+#!/usr/bin/python
+'''
+
+'''
+
+
+'''
+'''
+def determine_flavour(tweet):
+	pass #@todo implement this
+
+
+'''
+preconditions: @param tweet is a hateful message
+postconditions: returns tweet converted to unflavoured hate
+'''
+def unflavour(tweet):
+	flavour = determine_flavour(tweet)
+	#@todo implement this
+
+
+'''
+preconditions: @param tweet is an unflavoured hate, @param flavour is a valid hate flavour
+postconditions: returns the tweet converted to hate of the specified flavour
+				throws some exception if given flavoured hate
+'''
+def flavourize(tweet, flavour):
+	pass #@todo implement this
+
+
+'''
+preconditions: @param tweet is a hateful message, @param flavour is a valid flavour of hate
+postconditions: returns tweet converted to the specified flavour
+'''
+def reflavour(tweet, flavour):
+	pass #@todo implement this
