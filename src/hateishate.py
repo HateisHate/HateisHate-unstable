c27d3bfd99c2321e9d870d48a359ffdfb81ac11f
Aaron Thomas <1d20m6@gmail.com>
2016-11-05 17:01:40 -0400

    basic api stuff and pseudocode in the main loop

--- src/hateishate.py
+++ src/hateishate.py
@@ -1,1 +1,19 @@
 #!/user/bin/python
+import tweepy, time, sys
+import config
+ 
+#put your keys in config.py
+CONSUMER_KEY = config.CONSUMER_KEY
+CONSUMER_SECRET = config.CONSUMER_SECRET
+ACCESS_KEY = config.ACCESS_KEY
+ACCESS_SECRET = config.ACCESS_SECRET
+
+auth = tweepy.OAuthHandler(CONSUMER_KEY, CONSUMER_SECRET)
+auth.set_access_token(ACCESS_KEY, ACCESS_SECRET)
+api = tweepy.API(auth)
+
+while True:
+	pass
+	#check for mentions
+	#convert the hate
+	#reply to the mentioning tweet
