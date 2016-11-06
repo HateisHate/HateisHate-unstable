dd2734d0f8527946a0d8be14c7d032242e91cd58
JakeKillsAll <dabossjake1@gmail.com>
2016-11-06 10:18:45 -0500

    Almost done!!

--- src/hateishate.py
+++ src/hateishate.py
@@ -1,19 +1,37 @@
-#!/user/bin/python
-import tweepy, time, sys
+import tweepy
 import config
- 
-#put your keys in config.py
-CONSUMER_KEY = config.CONSUMER_KEY
-CONSUMER_SECRET = config.CONSUMER_SECRET
-ACCESS_KEY = config.ACCESS_KEY
-ACCESS_SECRET = config.ACCESS_SECRET
+import flavours
 
-auth = tweepy.OAuthHandler(CONSUMER_KEY, CONSUMER_SECRET)
-auth.set_access_token(ACCESS_KEY, ACCESS_SECRET)
+
+auth = tweepy.OAuthHandler(config.CONSUMER_KEY, config.CONSUMER_SECRET)
+auth.set_access_token(config.ACCESS_KEY, config.ACCESS_SECRET)
 api = tweepy.API(auth)
-
-while True:
-	pass
-	#check for mentions
-	#convert the hate
-	#reply to the mentioning tweet
+sampleValue = 0
+class StdOutListener(tweepy.StreamListener):
+    ''' Handles data received from the stream. '''
+    def on_status(self, status):
+        # Prints the text of the tweet
+        print('Tweet text: ' + status.text)
+        #if "t.co" in status.text:
+            #gets retweeted tweet contents
+        
+        tweet = flavours.reflavour(status.text)
+        tweet = tweet.replace('@hateishate_', '@' + status.user.screen_name)
+        tweet = tweet + ' #AllHateIsEqual'
+        print(tweet)
+		
+		
+    def on_error(self, status_code):
+        print("on_error")
+        print('Got an error with status code: ' + str(status_code))
+        return True # To continue listening
+ 
+    def on_timeout(self):
+        print('Timeout...')
+        return True # To continue listening
+		
+if __name__ == '__main__':
+	listener = StdOutListener()
+	print("ping!")
+	stream = tweepy.Stream(auth, listener)
+	stream.filter(track=['@hateishate_'])
