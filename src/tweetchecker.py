f09e32268f292bb554b81edd2d3378f1d067891c
Aaron Thomas <1d20m6@gmail.com>
2016-11-09 17:40:29 -0500

    fixed a bug that caused the bot to mistakenly tweet to some australian CEO

--- src/tweetchecker.py
+++ src/tweetchecker.py
@@ -3,8 +3,8 @@ import flavours
 
 def checker(tweet, status):
     tweet = flavours.reflavour(status.text)
-    tweet = tweet.replace('@hateishate_', '{}{}'.format('@',status.user.screen_name))
     tweet = tweet.replace('@hateishate_', '')
+    tweet = '{}{}'.format(tweet,status.user.screen_name)
     if len(tweet) > 121:
         tweet = '{}{}'.format(tweet[:121],'...')
     tweet = tweet + ' #AllHateIsEqual'
