0ac59ebbd220b219afb5f7f1612a0dad4b9f5331
Aaron Thomas <1d20m6@gmail.com>
2016-11-14 16:54:48 -0500

    Removed an unused import and fixed a bug that would cause the bot to tag wrong people sometimes.
    tweepy wasn't used in this module so I removed the unused import, a good slight micro-optimization
    The checker function was improperly concatenating the username to reply to at the end of the tweet instead of the beginning which caused it to be sometimes cut off (or omitted completely) when the tweet was longer than 121 characters. Removing part of a twitter name changes who is mentioned which can have some bad consequences when tweeting intentionally hateful material.
    The checker function now concatenates the username to the front of the tweet causing the name to never be cutoff.
    
    Issues that still exist:
    If someone's twitter name is tagged at the end of the tweet it can still get cut off

--- src/tweetchecker.py
+++ src/tweetchecker.py
@@ -1,11 +1,10 @@
-import tweepy
 import flavours
 
 def checker(tweet, status):
-    tweet = flavours.reflavour(status.text)
-    tweet = tweet.replace('@hateishate_', '')
-    tweet = '{}{}'.format(tweet,status.user.screen_name)
-    if len(tweet) > 121:
-        tweet = '{}{}'.format(tweet[:121],'...')
-    tweet = tweet + ' #AllHateIsEqual'
-    return(tweet)
+	tweet = flavours.reflavour(status.text)
+	tweet = tweet.replace('@hateishate_', '')
+	tweet = '{}{}'.format(status.user.screen_name,tweet)
+	if len(tweet) > 121:
+		tweet = '{}{}'.format(tweet[:121],'...')
+	tweet = tweet + ' #AllHateIsEqual'
+	return(tweet)
