54147525b27398c4bde9d42b02c2a3edf1c64702
Aaron Thomas <1d20m6@gmail.com>
2016-11-15 17:01:59 -0500

    Fixed an issue where the bot would not tag those who mention it
    Previously the bot would not tag the user who mentioned it.
    Now the bot does that.
    "user <tweet>" has become "@user <tweet>"

--- src/tweetchecker.py
+++ src/tweetchecker.py
@@ -3,7 +3,7 @@ import flavours
 def checker(tweet, status): #this is a bad function name @todo(you) give it a better name
 	tweet = flavours.reflavour(status.text)
 	tweet = tweet.replace('@hateishate_', '')
-	tweet = '{}{}'.format(status.user.screen_name,tweet)
+	tweet = '@{}{}'.format(status.user.screen_name,tweet)
 	if len(tweet) > 121:
 		tweet = '{}{}'.format(tweet[:121],'...')
 	tweet = tweet + ' #AllHateIsEqual'
