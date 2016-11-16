ce0c8fa4fd75348f554dc52f69bee9c3eff3a7f1
Aaron Thomas <1d20m6@gmail.com>
2016-11-16 14:12:42 -0500

    Added plans for bullyranger branch
    See docstrings in bullyranger.py for info about the plans

---
+++ src/bullyranger.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python
+'''
+this module should contain functions and shit that implement the anti bullying ideas I had
+tl;dr of the ideas
+open dms on the bot
+dm the username of a cyberbully
+every time the cyberbully posts an abusive tweet reply to them "Hey stop that!"
+use bark api to determine if the tweets are abusive
+if a tracked cyberbully stops being abusive then they will stop being tracked and the person(s) who originally reported them will be notified
+'''
+
+#todo(aaorn) code this
+#so we have to probably have a database for storing all the data about the people's hatefulness
+'''
+keep info about their average hate level
+some messages....
+You're always so hateful, can you tone it down?
+That's not nice!
+Hey, stop that!
+Don't bully!
+etc
+also it compares their current hate level to their average level. if they are at the highest level displays more intense messages
+'''
+
+
+'''
+WHEW!
+Looks like I'm going to actually need some practical dba knowledge of dba...
+Figure out how to structure the databse and then build the program on that.
+'''
