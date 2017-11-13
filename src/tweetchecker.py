a45910d73d1f3ef82fd2a2f3d6587b4c50e198b7
Aaron Thomas <1d20m6@gmail.com>
2017-11-13 17:50:53 -0500

    Why did past me think 140*2 was 240?

--- src/tweetchecker.py
+++ src/tweetchecker.py
@@ -2,7 +2,7 @@ import flavours
 
 def checker(status): #this is a bad function name @todo(you) give it a better name
 	hashtag = "#HateisHate"
-	maximum = 240 - (len(hashtag) + 4) #the 4 is a magic number, idk what it's for...
+	maximum = 280 - (len(hashtag) + 4) #the 4 is a magic number, idk what it's for...
 
 	tweet = flavours.reflavour(status.text)
 	if len(tweet) > maximum:
